94198c4ffec0cb1d728d4ed084baff2d7ef7db6a
zeryx <zeryx.ljh@gmail.com>
2018-07-29 19:18:42 +0800

    Added merchant module
    -Added merchantlogin.php
    -Added registermerchant.php

---
+++ merchantlogin.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once __DIR__ . '/config.php';
+
+// Create connection
+$con=mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
+
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$response = array();
+
+if (isset($_GET["user"]) && isset($_GET["pass"])) {
+	$user = $_GET['user'];
+	$pass = $_GET['pass'];
+
+	$pass = hash('sha256',$pass);
+
+	$sql = "SELECT * FROM merchants WHERE m_username = '$user' and m_password = '$pass'";
+
+	$result = mysqli_query($con, $sql);
+
+	if(mysqli_num_rows($result) != 0) {
+
+		$users = array();
+
+		while($row = $result->fetch_object())
+		{
+			$users = $row;
+		}
+
+		$response["code"] = 0;
+		$response["msg"] = "Login success";
+		$response["userInfo"] = array();
+
+		array_push($response["userInfo"], $users);
+
+		echo json_encode($response);
+	}
+	else {
+		$response["code"] = 2;
+		$response["msg"] = "Login failed";
+
+		echo json_encode($response);
+	}
+}
+
+else {
+	$response["code"] = 1;
+    $response["msg"] = "Required params missing";
+
+    echo json_encode($response);
+}
+
+// Close connections
+mysqli_close($con);
+?>
---
+++ registermerchant.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once __DIR__ . '/config.php';
+
+// Create connection
+$con=mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
+
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$response = array();
+
+if (isset($_GET["user"]) && isset($_GET["pass"]) && isset($_GET["mname"])) {
+	$user = $_GET['user'];
+	$pass = $_GET['pass'];
+	$mname = $_GET['mname'];
+
+	$pass = hash('sha256',$pass);
+
+	$sql = "INSERT INTO merchants VALUES (NULL,'$user','$pass','$mname',CURRENT_TIMESTAMP)";
+
+	$result = mysqli_query($con, $sql);
+
+
+	if($result) {
+		$response["code"] = 0;
+		$response["msg"] = "Register success";
+
+		echo json_encode($response);
+	}
+	else {
+		$response["code"] = mysqli_errno($con);
+		$response["msg"] = mysqli_error($con);
+
+		echo json_encode($response);
+	}
+}
+
+else {
+	$response["code"] = 1;
+    $response["msg"] = "Required params missing";
+
+    echo json_encode($response);
+}
+
+// Close connections
+mysqli_close($con);
+?>
